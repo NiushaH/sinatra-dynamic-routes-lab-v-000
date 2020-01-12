fb8efb869a43d34e91816835911ab54cd0551223
Niusha H <niusha.hadziomerovic@gmail.com>
2020-01-12 17:15:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-
+    number = params[:number].to_i
+    @phrase = params[:phrase]
+    number.times 
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
